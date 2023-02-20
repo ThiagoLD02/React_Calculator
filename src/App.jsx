a98844b34a058acf0859dcb2475bb94833a05bdd
Thiago <infothiago@unifei.edu.br>
2023-02-20 16:12:55 -0300

    Iniciando processo de logica

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -20,9 +21,73 @@ function App() {
 
   const keysRightGroup = ["*", "-", "+", "="];
 
-  const input = "6000/2+3227*2";
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
 
-  const result = "12,454";
+  function handleButtonClick(key) {
+    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    switch (key) {
+      case "Ac":
+        setInput("");
+        break;
+
+      case "Del":
+        setInput(input.slice(0, input.length - 1));
+        break;
+
+      case "=":
+        if (numbers.includes(input[input.length - 1]))
+          alert("Hora de calcular");
+        break;
+
+      case "/":
+      case "*":
+      case "+":
+      case "-":
+        if (numbers.includes(input[input.length - 1])) setInput(input + key);
+        break;
+
+      case ".":
+        if (numbers.includes(input[input.length - 1])) setInput(input + key);
+        break;
+
+      default:
+        setInput(input + key);
+        break;
+    }
+  }
+
+  function calculate(problem) {
+    const splittedProblem = problem.split(/([+\-*/])/);
+    let firstNumber = 0.0;
+    let secondNumber = 0.0;
+
+    let state = 0;
+
+    for (let index = 0; index < splittedProblem.length; index++) {
+      switch (state) {
+        case 0:
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+        case 5:
+          break;
+        case 6:
+          break;
+        case 7:
+          break;
+        case 8:
+          break;
+      }
+    }
+  }
 
   return (
     <div className="App">
@@ -30,25 +95,49 @@ function App() {
       <div className="CalculatorBox">
         <div className="Display">
           <p>{input}</p>
-          <h3>={result}</h3>
+          <h3>{result}</h3>
         </div>
         <div className="Keys">
           <div className="LeftGroup">
             <div className="Top">
               {keysLeftGroupTop.map((key) => {
-                return <button className={key}>{key}</button>;
+                return (
+                  <button
+                    onClick={() => handleButtonClick(key)}
+                    key={key}
+                    className={key}
+                  >
+                    {key}
+                  </button>
+                );
               })}
             </div>
 
             <div className="Bottom">
               {keysLeftGroupBottom.map((key) => {
-                return <button className={key}>{key}</button>;
+                return (
+                  <button
+                    onClick={() => handleButtonClick(key)}
+                    key={key}
+                    className={key}
+                  >
+                    {key}
+                  </button>
+                );
               })}
             </div>
           </div>
           <div className="RightGroup">
             {keysRightGroup.map((key) => {
-              return <button className={key}>{key}</button>;
+              return (
+                <button
+                  onClick={() => handleButtonClick(key)}
+                  key={key}
+                  className={key}
+                >
+                  {key}
+                </button>
+              );
             })}
           </div>
         </div>
