7f7f50e891393293ab40c521f797971b6c149be0
Thiago <infothiago@unifei.edu.br>
2023-02-20 21:09:52 -0300

    Ultimos ajustes na logica e estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -23,13 +23,31 @@ function App() {
 
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
+  const [clearInput, setClearInput] = useState(false);
+
+  function eraseInput() {}
 
   function handleButtonClick(key) {
     const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+    if (clearInput) {
+      if (numbers.includes(key)) {
+        setInput(key);
+        setClearInput(false);
+        return;
+      } else if (key !== "=" && key !== "Ac" && key !== "Del") {
+        setInput(result + key);
+        setClearInput(false);
+        return;
+      } else if (key == "Del") {
+        return;
+      }
+    }
+
     switch (key) {
       case "Ac":
         setInput("");
+        setResult("");
         break;
 
       case "Del":
@@ -37,8 +55,10 @@ function App() {
         break;
 
       case "=":
-        if (numbers.includes(input[input.length - 1]))
-          alert("Hora de calcular");
+        if (numbers.includes(input[input.length - 1])) {
+          setResult(calculate(input));
+          setClearInput(true);
+        }
         break;
 
       case "/":
@@ -61,32 +81,75 @@ function App() {
   function calculate(problem) {
     const splittedProblem = problem.split(/([+\-*/])/);
     let firstNumber = 0.0;
+    let firstOperator = "";
     let secondNumber = 0.0;
+    let secondOperator = "";
 
-    let state = 0;
-
-    for (let index = 0; index < splittedProblem.length; index++) {
+    let state = 1;
+    let index = 0;
+    let finished = false;
+    while (!finished) {
       switch (state) {
-        case 0:
-          break;
         case 1:
+          firstNumber = parseFloat(splittedProblem[index++]);
+          state = 2;
           break;
         case 2:
+          if (index === splittedProblem.length) {
+            state = 8;
+            break;
+          }
+          firstOperator = splittedProblem[index++];
+          if (firstOperator === "/" || firstOperator === "*") state = 3;
+          else state = 4;
           break;
         case 3:
+          secondNumber = parseFloat(splittedProblem[index++]);
+          if (firstOperator === "/") firstNumber = firstNumber / secondNumber;
+          else firstNumber = firstNumber * secondNumber;
+          secondNumber = 0.0;
+          firstOperator = "";
+          state = 2;
           break;
         case 4:
+          secondNumber = parseFloat(splittedProblem[index++]);
+          state = 5;
           break;
         case 5:
+          if (index === splittedProblem.length) {
+            state = 8;
+            break;
+          }
+          secondOperator = splittedProblem[index++];
+          if (secondOperator === "/" || secondOperator === "*") state = 7;
+          else state = 6;
           break;
         case 6:
+          if (firstOperator === "+") firstNumber = firstNumber + secondNumber;
+          else firstNumber = firstNumber - secondNumber;
+          secondNumber = 0.0;
+          firstOperator = secondOperator;
+          secondOperator = "";
+          if (firstOperator === "/" || firstOperator === "*") state = 3;
+          else state = 4;
           break;
         case 7:
+          const aux = parseFloat(splittedProblem[index++]);
+          if (secondOperator === "/") secondNumber = secondNumber / aux;
+          else secondNumber = secondNumber * aux;
+          state = 5;
           break;
         case 8:
+          if (firstOperator === "/") firstNumber = firstNumber / secondNumber;
+          if (firstOperator === "*") firstNumber = firstNumber * secondNumber;
+          if (firstOperator === "+") firstNumber = firstNumber + secondNumber;
+          if (firstOperator === "-") firstNumber = firstNumber - secondNumber;
+          finished = true;
+
           break;
       }
     }
+    return firstNumber;
   }
 
   return (
@@ -102,11 +165,7 @@ function App() {
             <div className="Top">
               {keysLeftGroupTop.map((key) => {
                 return (
-                  <button
-                    onClick={() => handleButtonClick(key)}
-                    key={key}
-                    className={key}
-                  >
+                  <button onClick={() => handleButtonClick(key)} key={key}>
                     {key}
                   </button>
                 );
@@ -116,11 +175,7 @@ function App() {
             <div className="Bottom">
               {keysLeftGroupBottom.map((key) => {
                 return (
-                  <button
-                    onClick={() => handleButtonClick(key)}
-                    key={key}
-                    className={key}
-                  >
+                  <button onClick={() => handleButtonClick(key)} key={key}>
                     {key}
                   </button>
                 );
@@ -130,11 +185,7 @@ function App() {
           <div className="RightGroup">
             {keysRightGroup.map((key) => {
               return (
-                <button
-                  onClick={() => handleButtonClick(key)}
-                  key={key}
-                  className={key}
-                >
+                <button onClick={() => handleButtonClick(key)} key={key}>
                   {key}
                 </button>
               );
